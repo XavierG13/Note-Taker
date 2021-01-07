0bc98f59a8363ff4e6f5318bc3c3a85db0d942ff
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-07 17:55:27 -0600

    added items for homework and depndencies in server.js

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,16 @@
+//Dependencies
+//======================================
+
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.join());
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
