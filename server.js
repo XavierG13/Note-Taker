503788ed604a76cb2f1f52d4bd75110b5b85dfc8
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-08 22:50:51 -0600

    added comments for express app and routes. also add js to the routes for notes and index html

--- server.js
+++ server.js
@@ -5,21 +5,37 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+// Sets up and the Express app and fs
+// =====================================
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Sets up Express app data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.join());
-app.use(express.static(path.join(__dirname, "assets/html")));
+// app.use(express.static(path.join(__dirname, "assets/html")));
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
 
+// Array to hold notes
 let notes = [];
 
+// Routes
+//======================================
+
+//Sends the user to AJAX page
 app.get("/notes", function (req, res) {
-  res.sendfile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-app.get();
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
