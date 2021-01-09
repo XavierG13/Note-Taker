a630c6b3c16f6cac230173a7b3d795b47d3871b4
Xavier Guzman <guzmanxavi112@gmail.com>
2021-01-09 01:06:38 -0600

    removed duplicate listen for server, added post for notes input

--- server.js
+++ server.js
@@ -14,12 +14,8 @@ const PORT = process.env.PORT || 3000;
 //Sets up Express app data parsing
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.join());
-// app.use(express.static(path.join(__dirname, "assets/html")));
-
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+app.use(express.static(path.join(__dirname, "assets/html")));
+app.use(express.json());
 
 // Array to hold notes
 
@@ -33,12 +29,24 @@ app.get("/notes", function (req, res) {
 });
 
 // Will default to home when not matching route is find
+
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/api/notes", function (req, res) {
   return res.sendFile(path.json(__dirname, "assets/db/db.json"));
+});
+
+// Takes in JSON notes input
+app.post("/api/notes", function (req, res) {
+  fs.readFile("assets/js/index.js", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed", err);
+      return notes;
+    }
+    console.log("File data:", jsonString);
+  });
 });
 
 //Begins listening to the server
